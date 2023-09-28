1f7bd4c54f4067fff98a7d0f05fa2a8f6f893244
iowauclawin <iowaucla@gmail.com>
2023-09-27 22:40:43 -0700

    Comments on Drive Train

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -25,49 +25,49 @@ public class DriveTrain extends SubsystemBase
   private final WPI_TalonSRX leftDriveTalon;
   private final WPI_TalonSRX rightDriveTalon;
 
-  private AHRS navx = new AHRS(SPI.Port.kMXP);
+  private AHRS navx = new AHRS(SPI.Port.kMXP); //This is a constructor, which creates an object in the AHRS class called navx
 
-  private ShuffleboardTab DTTab = Shuffleboard.getTab("DriveTrain");
-  private GenericEntry LeftVoltage = DTTab.add("Left Voltage", 0.0).getEntry();
-  private GenericEntry RightVoltage = DTTab.add("Right Voltage", 0.0).getEntry();
+  private ShuffleboardTab DTTab = Shuffleboard.getTab("DriveTrain"); //This titles the variable DTTab into DriveTrain
+  private GenericEntry LeftVoltage = DTTab.add("Left Voltage", 0.0).getEntry(); //Updates DTTab with Left Voltage with the value of 0.0 and stores the return value of getEntry into LeftVoltage
+  private GenericEntry RightVoltage = DTTab.add("Right Voltage", 0.0).getEntry(); //Updates DTTab with Right Voltage with the value of 0.0 and stores the return value of getEntry into RightVoltage
 
   /** Creates a new DriveTrain */
   public DriveTrain() 
   {
-    leftDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.LeftDriveTalonPort);
-    rightDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.RightDriveTalonPort);
+    leftDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.LeftDriveTalonPort); //Creating an object by using a constructor for leftDriveTalon
+    rightDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.RightDriveTalonPort); //Same as above except for rightDriveTalon
   
-    leftDriveTalon.setNeutralMode(NeutralMode.Coast);
-    rightDriveTalon.setNeutralMode(NeutralMode.Coast);
+    leftDriveTalon.setNeutralMode(NeutralMode.Coast); //Sets leftDriveTalon to neutral
+    rightDriveTalon.setNeutralMode(NeutralMode.Coast); //Sets rightDriveTalon to neutral
 
-    leftDriveTalon.setInverted(true);
-    rightDriveTalon.setInverted(false);
+    leftDriveTalon.setInverted(true); //Make sure the leftDriveTalon is inverted compared to the rightDriveTalon so it can move forward and backword correctly
+    rightDriveTalon.setInverted(false); //Make sure the rightDriveTalon is inverted compared to the leftDriveTalon so it can move forward and backword correctly
 
-    leftDriveTalon.setSensorPhase(true);
-    rightDriveTalon.setSensorPhase(true);
+    leftDriveTalon.setSensorPhase(true); //Sets the sensors for leftDriveTalon to true
+    rightDriveTalon.setSensorPhase(true); //Sets the sensors for rightDriveTalon to true
 
-    leftDriveTalon.configFactoryDefault();
-    leftDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
-    rightDriveTalon.configFactoryDefault();
+    leftDriveTalon.configFactoryDefault(); //resets leftDriveTalon
+    leftDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10); //Sets up encoder to track rotation of motor
+    rightDriveTalon.configFactoryDefault(); //resets rightDriveTalon
     rightDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
 
   }
 
-  public void tankDrive(double leftSpeed, double rightSpeed) {
+  public void tankDrive(double leftSpeed, double rightSpeed) { //This will drive the robot with a certain speed
     rightDriveTalon.set(rightSpeed);
     leftDriveTalon.set(leftSpeed);
   }
 
   public void resetEncoders() {
-    leftDriveTalon.setSelectedSensorPosition(0,0,10);
-    rightDriveTalon.setSelectedSensorPosition(0,0,10);
+    leftDriveTalon.setSelectedSensorPosition(0,0,10); //Sets sensor position to 0,0,10
+    rightDriveTalon.setSelectedSensorPosition(0,0,10); //Sets sensor position to 0,0,10
   }
 
   public double getTicks() {
     return (leftDriveTalon.getSelectedSensorPosition(0) + rightDriveTalon.getSelectedSensorPosition(0)) / 2.0;
   }
  
-  public double getAngle(){
+  public double getAngle(){ //Gets the robot's current angle
     return navx.getAngle(); 
   }
  
